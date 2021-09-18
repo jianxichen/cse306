a1fc9f766e1cae8344c82ab8f1f7e7b24bc66021
Jian Xi Chen <jianxi.chen@stonybrook.edu>
2021-09-18 00:42:30 -0400

    mouseinit() and main.c

--- kernel/mouse.c
+++ kernel/mouse.c
@@ -4,5 +4,25 @@
 #include "kbd.h"
 
 void mouseinit(){
+    // Check and wait till controller is ready
+    while((inb(KBSTATP)&0x2)==0){
+        cprintf("PS2 controller status not ready");
+    }
+    // Issue compaq byte to controller to activate IRQ12
+    outb(KBSTATP, 0x20);
+
+    // Read status byte back
+    uchar st=inb(KBSTATP);
+
+    // Modify status byte
+    st=st|0x2;
+    st=st&0x20;
+
+    // Issue compaq byte and modified databyte
+    outb(KBSTATP, 0x20);
+    outb(KBDATAP, st);
+}
+
+void mouseintr(){
     
 }
