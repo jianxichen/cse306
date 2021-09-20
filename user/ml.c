521448c6a97a89d7ea90342dd6baa44376efb675
Jianming Tan <jianming.tan@stonybrook.edu>
2021-09-19 20:25:22 -0400

    exercise 3: demo

---
+++ user/ml.c
@@ -0,0 +1,14 @@
+/**
+ * ML (stand for Mouse Listener), a program that prints the mouse events.
+ */
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fs.h"
+#include "user.h"
+
+int main(int argc, char const *argv[])
+{
+  printf(0, "Hello Mouse!\n");
+  return 0;
+}
